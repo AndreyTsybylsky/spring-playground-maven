05381f34ed2e36711c970f173bcba86e60b45728
ubuntu <gucofff02@gmail.com>
2022-08-02 04:32:51 -0400

    model as a container for sending info to the html with tf

--- src/main/java/ru/tsybulski/springplayground/controllers/FirstController.java
+++ src/main/java/ru/tsybulski/springplayground/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.tsybulski.springplayground.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname){
-        System.out.println(name + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+//        System.out.println(name + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
